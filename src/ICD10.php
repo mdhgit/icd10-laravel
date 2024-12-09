a981c8cafb73e88e6b286112a19410e4f403b63c
Lynton DSouza <lynton.dsouza@mydiabeteshome.com>
2024-12-09 10:52:44 +0530

    ICD10 Code Liabrary

---
+++ src/ICD10.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Mydiabeteshome\ICD10;
+
+use Exception;
+
+class ICD10 extends ICD10ServiceProvider
+{
+
+    private $icd10Resource = 'https://clinicaltables.nlm.nih.gov/api/icd10cm/v3/search';
+
+
+    /**
+     * Objective of the function is to get ICD10 Codes via the US Government API
+     * @param int $search_term search code or title
+     * @param array $max_list limit result
+     * @param array $search_phase
+     */
+    public function getIcd10CodeData($search_term, $max_list = 99999999, $search_phase = 'code,name')
+    {
+        try {
+            $cURLConnection = curl_init($this->icd10Resource . '?sf=' . $search_phase . '&terms=' . $search_term . '&maxList=' . $max_list);
+            curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+            $apiResponse = curl_exec($cURLConnection);
+            curl_close($cURLConnection);
+            if (!empty($apiResponse)) {
+                $apiResponse = json_decode($apiResponse);
+                if (empty($apiResponse)) {
+                    $data = [];
+                    if (!empty($apiResponse[3])) {
+                        $codeList = $apiResponse[3];
+                        if (!empty($codeList)) {
+                            foreach ($codeList as $code) {
+                                if(!empty($code[0]) && !empty($code[1])){
+                                    $data[]=['code'=>$code[0],'title'=>$code[1]];
+                                }
+                            }
+                        }
+                    }
+                    return ['status' => 200, 'data' => $data];
+                } else {
+                    return ['status' => 404];
+                }
+            } else {
+                return ['status' => 404];
+            }
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage(), 500);
+        }
+    }
+}
---
+++ src/ICD10ServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Mydiabeteshome\ICD10;
+
+use Illuminate\Support\ServiceProvider;
+
+class ICD10ServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+        $this->app->singleton(ICD10::class, function () {
+            return new ICD10('client_id', 'bearer_token');
+        });
+    }
+}
